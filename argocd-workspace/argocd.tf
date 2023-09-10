6d18226e98aabe1938b389d016f4e3d6bed3539d
ung <ung@mz.co.kr>
2023-09-10 14:29:40 +0900

    포스의 힘

--- argocd-workspace/argocd.tf
+++ argocd-workspace/argocd.tf
@@ -80,8 +80,19 @@ resource "kubernetes_manifest" "secret_argocd_argocd_vault_plugin_credentials" {
   depends_on = [helm_release.argocd]  
 }
 
+resource "null_resource" "force_update" {
+  triggers = {
+    kubernetes_deployment_id = kubernetes_manifest.deployment_argocd_argocd_repo_server.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Force update triggered'"
+  }
+
+  depends_on = [kubernetes_manifest.deployment_argocd_argocd_repo_server]
+}
+
 resource "kubernetes_manifest" "deployment_argocd_argocd_repo_server" {
-  force_new = true
   manifest = {
     "apiVersion" = "apps/v1"
     "kind" = "Deployment"
