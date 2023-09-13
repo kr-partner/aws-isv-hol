2b9b2b66d6c84b351cda47770c65d3c41844188b
Hyungwook Yu <hyungwook.yu@hashicorp.com>
2023-09-13 20:44:49 +0900

    1m -> 60 변경

--- 6_vso-resource-workspace/dynamic-postgres.tf
+++ 6_vso-resource-workspace/dynamic-postgres.tf
@@ -61,8 +61,8 @@ resource "vault_database_secret_backend_role" "postgres" {
   backend   = vault_database_secrets_mount.db.path
   name      = var.db_role
   db_name   = vault_database_secrets_mount.db.postgresql[0].name
-  default_ttl = "1m"
-  max_ttl = "1m"
+  default_ttl = "60"
+  max_ttl = "60"
   creation_statements = [
     "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
     "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";",
